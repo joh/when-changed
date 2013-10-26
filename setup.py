861b04aa47601ffb03a28c50ba08025bbb29f449
Justin Mayhew <mayhew@live.ca>
2013-10-26 01:48:16 -0300

    Use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='When-changed',
+setup(name='when-changed',
       version='0.2.0',
       description='Run a command when a file is changed',
       author='Johannes H. Jensen',
       url='https://github.com/joh/when-changed',
       packages=['whenchanged'],
       scripts=['when-changed'],
-      requires=['pyinotify'],
+      install_requires=['pyinotify'],
       license='BSD'
       )
