6237b000a6e9bb79f88024b6bb6e80ee950a07dd
Johannes H. Jensen <joh@pseudoberries.com>
2014-04-21 22:24:53 +0200

    Switch to setuptools and use install_requires
    This fixes #17 and allows dependencies like pyinotify to be installed
    automatically.

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='when-changed',
-      version='0.2.0',
+      version='0.2.1',
       description='Run a command when a file is changed',
       author='Johannes H. Jensen',
       author_email='joh@pseudoberries.com',
       url='https://github.com/joh/when-changed',
       packages=['whenchanged'],
       scripts=['when-changed'],
-      requires=['pyinotify'],
+      install_requires=['pyinotify'],
       license='BSD'
       )
